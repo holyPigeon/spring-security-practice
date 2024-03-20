d218d4b9000b9434094a4225380aaf86f3299fae
jae woo Jung <awfjol2008@naver.com>
2024-03-20 16:15:42 +0900

    feat: 회원가입 메소드 구현

--- backend/src/main/java/com/kyonggi/springsecuritypractice/service/UserService.java
+++ backend/src/main/java/com/kyonggi/springsecuritypractice/service/UserService.java
@@ -1,10 +1,13 @@
 package com.kyonggi.springsecuritypractice.service;
 
+import com.kyonggi.springsecuritypractice.domain.User;
 import com.kyonggi.springsecuritypractice.repository.UserRepository;
+import com.kyonggi.springsecuritypractice.service.dto.AddUserRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +15,16 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(
+                User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()
+        ).getId();
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
