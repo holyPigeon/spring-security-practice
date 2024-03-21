5ba44a80a15194dd7e82c652cc91cf9f5ff1a1f4
jae woo Jung <awfjol2008@naver.com>
2024-03-21 14:11:55 +0900

    feat feat: UserService 및 UserDetailService 분리

--- backend/src/main/java/com/kyonggi/springsecuritypractice/service/UserService.java
+++ backend/src/main/java/com/kyonggi/springsecuritypractice/service/UserService.java
@@ -4,15 +4,12 @@ import com.kyonggi.springsecuritypractice.domain.User;
 import com.kyonggi.springsecuritypractice.repository.UserRepository;
 import com.kyonggi.springsecuritypractice.service.dto.AddUserRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService{
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -20,15 +17,9 @@ public class UserService implements UserDetailsService {
     public Long save(AddUserRequest dto) {
         return userRepository.save(
                 User.builder()
-                .email(dto.getEmail())
+                .email(dto.getLoginId())
                 .password(bCryptPasswordEncoder.encode(dto.getPassword()))
                 .build()
         ).getId();
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("잘못된 이메일입니다."));
     }
 }
